fa494ae46cb6f18329532ea9a1d3055be09fda1f
msciborski <michal.sciborski@student.put.poznan.pl>
2018-09-09 10:04:38 +0200

    Add fetch book manager.

---
+++ src/APIFetch.js
@@ -0,0 +1,53 @@
+const rp = require('request-promise');
+const cherio = require('cherio');
+
+const BASE_URL = 'http://lubimyczytac.pl/szukaj/ksiazki?phrase=';
+
+const setupOptions = (method = 'GET', uri, queryStrings = {}) => ({
+  uri,
+  qs: queryStrings,
+  method,
+  transform: body => cherio.load(body),
+});
+
+const filterUrls = urls => urls.filter(url => url.match(/\/ksiazka\//));
+
+const fetchBookUrlsForPhrase = phrase => rp(setupOptions('GET', BASE_URL, { phrase }))
+  .then(($) => {
+    const results = $('.books-list .bookTitle')
+      .map((_, el) => $(el).attr('href')).get();
+    return filterUrls(results);
+  });
+
+const fetchBookDetails = ($) => {
+  const isbn = $('dd span[itemprop="isbn"]').text();
+  const publishingDate = $('dd span[itemprop="datePublished"]').attr('content');
+
+  return {
+    isbn,
+    publishingDate,
+  };
+};
+
+const fetchBook = url => rp(setupOptions('GET', url))
+  .then(($) => {
+    const title = $('h1[itemprop="name"] a').text();
+    const author = $('.author-info-container a[itemprop="name"]').map((_, el) => $(el).text()).get().join(' ,');
+    const bookDetails = fetchBookDetails($);
+    return {
+      title,
+      author,
+      isbn: bookDetails.isbn,
+      publishingDate: bookDetails.publishingDate,
+    };
+  });
+
+const getBooks = phrase => fetchBookUrlsForPhrase(phrase)
+  .then((urls) => {
+    const books = [];
+    urls.forEach(url => books.push(Promise.resolve(fetchBook(url))));
+    return books;
+  });
+
+
+module.exports.getBooks = getBooks;
--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
-const argv = require('yargs')
+const { argv } = require('yargs')
   .command('search', 'Search book in lubimyczytac.pl', {
-  phrase: {
-    describe: 'Phrase to search',
-    demand: true,
-    alias: 'p'
+    phrase: {
+      describe: 'Phrase to search',
+      demand: true,
+      alias: 'p',
+    },
+  });
+
+const APIFetch = require('./APIFetch');
+
+const command = argv._[0];
+
+switch (command) {
+  case 'search': {
+    const { phrase } = argv;
+    APIFetch.getBooks(phrase)
+      .then(books => Promise.all(books)
+        .then(book => console.log(book)));
+    break;
   }
-}).argv;
-
-
-  const command = argv._[0];
-
-  switch(command) {
-    case 'search':
-      break;
-    default:
-      console.log(`Command ${command} not found.`);
-      break;
-  }
+  default:
+    console.log(`Command ${command} not found.`);
+    break;
+}
