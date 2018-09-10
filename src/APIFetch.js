d8e93787d70b44cb0cf89a2551fbf9f63ed6b3be
msciborski <michal.sciborski@student.put.poznan.pl>
2018-09-10 23:09:22 +0200

    Refactoring, creating methods responsible for fetching next and prev page

--- src/APIFetch.js
+++ src/APIFetch.js
@@ -12,11 +12,32 @@ const setupOptions = (method = 'GET', uri, queryStrings = {}) => ({
 
 const filterUrls = urls => urls.filter(url => url.match(/\/ksiazka\//));
 
+const fetchBookUrls = ($) => {
+  const results = $('.books-list .bookTitle')
+    .map((_, el) => $(el).attr('href')).get();
+  return filterUrls(results);
+};
+
+const fetchNextPageUrl = ($) => {
+  const nextPageUrl = $('.pager-default li.next-page a').attr('href');
+  return nextPageUrl === '#' ? null : nextPageUrl;
+};
+
+const fetchPrevPageUrl = ($) => {
+  const prevPageUrl = $('.pager-default li.prev-page a').attr('href');
+  return prevPageUrl === '#' ? null : prevPageUrl;
+};
+
 const fetchBookUrlsForPhrase = phrase => rp(setupOptions('GET', BASE_URL, { phrase }))
   .then(($) => {
-    const results = $('.books-list .bookTitle')
-      .map((_, el) => $(el).attr('href')).get();
-    return filterUrls(results);
+    const bookUrls = fetchBookUrls($);
+    const nextPageUrl = fetchNextPageUrl($);
+    const prevPageUrl = fetchPrevPageUrl($);
+    return {
+      books: bookUrls,
+      prevPageUrl,
+      nextPageUrl,
+    };
   });
 
 const fetchBookDetails = ($) => {
@@ -43,7 +64,7 @@ const fetchBook = url => rp(setupOptions('GET', url))
   });
 
 const getBooks = phrase => fetchBookUrlsForPhrase(phrase)
-  .then(urls => Promise.all(urls.map(url => fetchBook(url))));
+  .then(urls => Promise.all(urls.books.map(url => fetchBook(url))));
 
 
 module.exports.getBooks = getBooks;
